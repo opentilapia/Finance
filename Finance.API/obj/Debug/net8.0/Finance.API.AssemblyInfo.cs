4b4345efb81da2ffd18bc71d380068280aff5d0e
Ger <11022354+opentilapia@users.noreply.github.com>
2025-05-14 23:01:38 +0800

    add to gitignore

--- Finance.API/obj/Debug/net8.0/Finance.API.AssemblyInfo.cs
+++ Finance.API/obj/Debug/net8.0/Finance.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finance.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c198c11fccd6e41f66ecf80039e3e885e2cfc68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finance.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finance.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
